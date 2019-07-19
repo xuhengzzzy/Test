0e1751ce854a782e9a5da74ed6a97afc6bcf5517
hengdidi <634455291@qq.com>
2019-07-19 12:41:15 +0800

    Second upload

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    //111111
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
